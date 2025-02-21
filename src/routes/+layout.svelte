4feaf12d81264d394cc9816ef738f266b3f3cbb0
Didier Catz <didierlennon@gmail.com>
2025-02-21 12:55:13 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../app.css'
+
+	let { children } = $props()
+</script>
+
+<nav class="px container mx-auto grid-cols-2 content-between justify-between py-4 lg:grid">
+	<div class="flex justify-start text-xl">
+		<a href="/">Home</a>
+	</div>
+	<div class="flex justify-end">
+		<a
+			href="/"
+			class="bg-primary-600 dark:bg-primary-600 rounded-xl px-4 py-2.5 font-medium text-white"
+			>Get started</a
+		>
+	</div>
+</nav>
+
+<div class="px container mx-auto mt-20">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<header>
+	<h1 class="title-1">
+		Your US Visa, <span class="text-emphasis-medium block">ten times faster and cheaper.</span>
+	</h1>
+
+	<p class="text-emphasis-low">
+		COMPANY makes getting your US visa fast and cheap, saving you struggles getting into the US
+	</p>
+</header>
