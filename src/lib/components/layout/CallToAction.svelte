310eb383aa3714fe0040d4efed6a2677ca860e6c
Didier Catz <didierlennon@gmail.com>
2025-03-21 14:46:40 +0100

    update template

--- src/lib/components/layout/CallToAction.svelte
+++ src/lib/components/layout/CallToAction.svelte
@@ -62,7 +62,7 @@
 			<img
 				src="https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=80"
 				alt="Visual comparison showing product benefits"
-				class=" hidden h-full max-h-full w-full rounded-[calc(max(var(--inner-radius),.25rem))] object-cover lg:block"
+				class="hidden aspect-[4/5] size-full max-h-full w-full rounded-[calc(max(var(--inner-radius),.25rem))] object-cover lg:block"
 			/>
 		</div>
 	</section>
