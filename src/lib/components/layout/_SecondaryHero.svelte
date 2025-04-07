f679a2debddf1e5e6f769cc18def8a013b34a3db
Didier Catz <didierlennon@gmail.com>
2025-04-07 16:15:44 +0200

    Add UI components for team and career sections

---
+++ src/lib/components/layout/_SecondaryHero.svelte
@@ -0,0 +1,16 @@
+<script>
+	// Components
+	import AnimateText from "../animation/AnimateText.svelte";
+
+	let { title = "" } = $props();
+</script>
+
+<!-- Hero Section -->
+<section class="bg-gray-50">
+	<div class="section-py section-px container mx-auto grid min-h-[50vh] gap-16">
+		<!-- <div class="items-between grid gap-6 lg:grid-cols-2"> -->
+		<h1 class="text-display text-emphasis-high">
+			<AnimateText text={title} />
+		</h1>
+	</div>
+</section>
---
+++ src/routes/about/StakeholderGrid.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+	import SectionHeader from "$lib/components/layout/SectionHeader.svelte";
+	import Button from "$lib/components/ui/Button.svelte";
+	import { animate, stagger } from "motion";
+	import { onMount } from "svelte";
+
+	// Types
+	type TeamMember = {
+		name: string;
+		position: string;
+		bio: string;
+		image: string;
+	};
+
+	// Props
+	const {
+		title,
+		stakeholderType = "team",
+		subtitle,
+		stakeholders
+	}: {
+		stakeholders: TeamMember[];
+		title: string;
+		subtitle: string;
+		stakeholderType?: "team" | "investor";
+	} = $props();
+
+	let cards: HTMLElement[] = $state([]);
+
+	onMount(() => {
+		animate(
+			cards,
+			{
+				y: ["1.5rem", 0],
+				filter: ["blur(2px)", "blur(0px)"],
+				opacity: [0, 1]
+			},
+			{
+				duration: 0.3,
+				ease: "easeOut",
+				delay: stagger(0.1, {
+					ease: "easeInOut"
+				})
+			}
+		);
+	});
+</script>
+
+<section class="bg-white">
+	<div
+		class="section-py section-px container mx-auto grid [--gap:--spacing(4)] [--radius:var(--radius-2xl)]"
+	>
+		<SectionHeader {title} {subtitle} />
+
+		<div
+			class="grid gap-(--gap)"
+			style:grid-template-columns="repeat(auto-fit, minmax(200px, 1fr))"
+		>
+			{#snippet card(
+				{
+					name,
+					position,
+					image,
+					type = "team"
+				}: {
+					name: string;
+					position?: string;
+					image: string;
+					type?: "team" | "join";
+				},
+				index: number
+			)}
+				<div
+					bind:this={cards[index]}
+					class={[
+						"card bg-gray-50 opacity-0",
+						"relative isolate grid aspect-[4/5] content-end items-end gap-10 overflow-hidden rounded-(--radius)  bg-cover p-(--gap) [--inner-radius:calc(var(--radius)-var(--gap))] ",
+						type === "join" ? "bg-primary-400" : ""
+					]}
+					style={type === "join" ? "" : `background-image: url('${image}')`}
+				>
+					{#if type === "team" && stakeholderType === "team"}
+						<div
+							class="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-black/0 via-black/0 to-black/30"
+						>
+							<div class="linear-blur-down absolute right-0 bottom-0 left-0 h-[25%]"></div>
+						</div>
+					{/if}
+
+					<div class="text-caption z-10 text-white">
+						<div class={type === "join" ? "grid grid-cols-2 items-center" : ""}>
+							<div>
+								<div>{name}</div>
+								<div class="opacity-70">{position}</div>
+							</div>
+							{#if type === "join"}
+								<div class="justify-self-end">
+									<Button href="/careers" variant="secondary" size="sm">Join us</Button>
+								</div>
+							{/if}
+						</div>
+					</div>
+				</div>
+			{/snippet}
+
+			{#each stakeholders as member, index}
+				{@render card(member, index)}
+			{/each}
+
+			{#if stakeholderType === "team"}
+				{@render card(
+					{
+						name: "You?",
+						position: "Join us",
+						type: "join"
+					},
+					stakeholders.length
+				)}
+			{/if}
+		</div>
+	</div>
+</section>
