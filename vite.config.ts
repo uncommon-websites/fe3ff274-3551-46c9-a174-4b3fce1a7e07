99528de1afdcfd4fdf7df61e38130757a26c30e8
Didier Catz <didierlennon@gmail.com>
2025-04-14 15:06:12 +0200

    remove overlay in prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,23 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import Icons from "unplugin-icons/vite";
 
-export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		Icons({
-			compiler: "svelte"
-		})
-	],
-	server: {
-		allowedHosts: [".unc.mn", "localhost"]
-	}
+export default defineConfig(({ mode }) => {
+	// Check if we're in development mode
+	const dev = mode === "development";
+
+	return {
+		plugins: [
+			tailwindcss(),
+			sveltekit(),
+			Icons({
+				compiler: "svelte"
+			})
+		],
+		server: {
+			allowedHosts: [".unc.mn", "localhost"],
+			hmr: {
+				overlay: dev
+			}
+		}
+	};
 });
