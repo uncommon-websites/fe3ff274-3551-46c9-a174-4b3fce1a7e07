c724a53f759c2c9e06fa424b936983b13559b9d7
Didier Catz <didierlennon@gmail.com>
2025-04-14 23:33:11 +0200

    fix build

--- src/routes/legal/[type]/+page.svelte
+++ src/routes/legal/[type]/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-	// Types
-	import type { PageData } from "./$types";
-
 	// Components
-	import { METADATA } from "$lib/content";
+	import { CONFIG } from "$lib/content";
 
 	// Props
 	let { data } = $props();
 </script>
 
 <svelte:head>
-	<title>{data.meta.title} | {METADATA.companyName}</title>
+	<title>{data.meta.title} | {CONFIG.companyName}</title>
 	<meta name="description" content={data.meta.description} />
 </svelte:head>
 <div class="section-px section-py container mx-auto max-w-prose">
